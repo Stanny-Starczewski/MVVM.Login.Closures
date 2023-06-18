d7352b6b447b888206cfc351ad6e6703387a9f20
Stanny-Starczewski <spaceboyua@yahoo.com>
2023-06-18 03:09:48 -0400

    приходят динамически определяемые цвета

--- MVVM.Login.Closures/Helpers/Colors.swift
+++ MVVM.Login.Closures/Helpers/Colors.swift
@@ -2,4 +2,12 @@ import UIKit
 
 final class Colors {
     let viewBackgroundColor = UIColor.systemBackground
+    
+    let buttonDisabledColor = UIColor { (traits: UITraitCollection) -> UIColor in
+        if traits.userInterfaceStyle == .light {
+            return UIColor.lightGray                                    // светлый режим
+        } else {
+            return UIColor(red: 0.8, green: 0.5, blue: 0.8, alpha: 1)   // тёмный режим
+        }
+    }
 }
--- MVVM.Login.Closures/View/LoginViewController.swift
+++ MVVM.Login.Closures/View/LoginViewController.swift
@@ -43,7 +43,7 @@ final class LoginViewController: UIViewController {
     
     private func setLoginButton(enabled: Bool) {
         loginButton.isUserInteractionEnabled = enabled
-        loginButton.backgroundColor = enabled ? .orange : .lightGray
+        loginButton.backgroundColor = enabled ? .orange : colors.buttonDisabledColor
     }
     
     private func setError(_ message: String?) {
